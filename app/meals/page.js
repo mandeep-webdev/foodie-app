8351a59b37b6d9b0135dd6f67054267f2caf6eba
Inderjit Singh <mandeep.fullstack.dev@gmail.com>
2025-01-06 15:04:45 +0530

    fetched the meals data from database and showed onto the screen

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,30 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import Link from "next/link";
 import React from "react";
-
-const Meals = () => {
-  return <div>Meals</div>;
+import styles from "./page.module.css";
+const Meals = async () => {
+  const meals = await getMeals();
+  // console.log(meals);
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and coot it yourself. It's easy and fun.
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 };
 
 export default Meals;
