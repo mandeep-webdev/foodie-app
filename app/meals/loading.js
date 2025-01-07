c2897b23d216dc06850c449332efd804093a8430
Inderjit Singh <mandeep.fullstack.dev@gmail.com>
2025-01-07 13:04:15 +0530

    set loading indicator

---
+++ app/meals/loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./loading.module.css";
+const LoadingPage = () => {
+  return <p className={styles.loading}>Fetching Meals...</p>;
+};
+
+export default LoadingPage;
