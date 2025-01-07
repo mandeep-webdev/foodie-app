a5860fb10a6b3c79ef38cdc9fee0c771ab8fdff7
Inderjit Singh <mandeep.fullstack.dev@gmail.com>
2025-01-07 13:38:33 +0530

    handling loading state with suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,18 @@
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 import styles from "./page.module.css";
-const Meals = async () => {
+
+const FetechingMeals = async () => {
   const meals = await getMeals();
-  // console.log(meals);
+  return (
+    <main>
+      <MealsGrid meals={meals} />
+    </main>
+  );
+};
+const Meals = () => {
   return (
     <>
       <header className={styles.header}>
@@ -20,9 +27,9 @@ const Meals = async () => {
           <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
-      <main>
-        <MealsGrid meals={meals} />
-      </main>
+      <Suspense fallback={<p className={styles.loading}>Fetching Meals...</p>}>
+        <FetechingMeals />
+      </Suspense>
     </>
   );
 };
